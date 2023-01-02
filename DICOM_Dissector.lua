c0bf9476e8c46d1b62d0575ad6c583e104c5fdac
pa4ul <paulherbich1@gmail.com>
2023-01-02 18:46:59 +0100

    Altered to dicom

--- DICOM_Dissector.lua
+++ DICOM_Dissector.lua
@@ -4,28 +4,34 @@
     -- echo "print('hello')" > test.lua && tshark -X lua_script:test.lua
     -- great source: https://mika-s.github.io/wireshark/lua/dissector/2017/11/04/creating-a-wireshark-dissector-in-lua-1.html
 
-    mongodb_protocol = Proto("MongoDB",  "MongoDB Protocol")
+    dicom_protocol = Proto("dicom-a",  "DICOM A-Type message")
 
-    message_length = ProtoField.int32("mongodb.message_length", "messageLength", base.DEC)
-    request_id     = ProtoField.int32("mongodb.requestid"     , "requestID"    , base.DEC)
-    response_to    = ProtoField.int32("mongodb.responseto"    , "responseTo"   , base.DEC)
-    opcode         = ProtoField.int32("mongodb.opcode"        , "opCode"       , base.DEC)
+    pdu_type = ProtoField.uint8("dicom-a.pdu_type","pduType",base.DEC,{
+      [1]="ASSOC Request",
+      [2]="ASSOC Accept",
+      [3]="ASSOC Reject",
+      [4]="DATA",
+      [5]="RELEASE Request",
+      [6]="RELEASE Response",
+      [7]="ABORT",
+    })
+
+    message_length = ProtoField.uint16("dicom-a.message_length", "messageLength", base.DEC)
     
-    mongodb_protocol.fields = { message_length, request_id, response_to, opcode }
+    dicom_protocol.fields = { message_length, pdu_type }
     
-    function mongodb_protocol.dissector(buffer, pinfo, tree)
+    function dicom_protocol.dissector(buffer, pinfo, tree)
       length = buffer:len()
       if length == 0 then return end
     
-      pinfo.cols.protocol = mongodb_protocol.name
+      pinfo.cols.protocol = dicom_protocol.name
     
-      local subtree = tree:add(mongodb_protocol, buffer(), "MongoDB Protocol Data")
+      local subtree = tree:add(dicom_protocol, buffer(), "DICOM PDU")
     
-      subtree:add_le(message_length, buffer(0,4))
-      subtree:add_le(request_id,     buffer(4,4))
-      subtree:add_le(response_to,    buffer(8,4))
-      subtree:add_le(opcode,         buffer(12,4))
+      subtree:add_le(pdu_type, buffer(0,1))
+      subtree:add(message_length, buffer(2,4))
+
     end
     
     local tcp_port = DissectorTable.get("tcp.port")
-    tcp_port:add(59274, mongodb_protocol)
+    tcp_port:add(4242, dicom_protocol)
